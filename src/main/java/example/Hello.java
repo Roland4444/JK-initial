004e32dfdf8fadae47be94c1fb0dc31ebb3464e1
Roland4444 <turbopollux@rambler.ru>
2021-02-12 04:58:09 +0400

    adding simple call high order function

--- src/main/java/example/Hello.java
+++ src/main/java/example/Hello.java
@@ -6,8 +6,8 @@ public class Hello {
     public static void main(String[] args){
         System.out.println("hello");
         var f = new Holla();
-        f.print();
-        new Holla().print();
+
         Holla.Companion.a();
+        f.callmarina();
     }
 }
--- src/main/kotlin/com/avs/Holla.kt
+++ src/main/kotlin/com/avs/Holla.kt
@@ -8,7 +8,19 @@ class Holla {
         println("Hello, World!")
     }
 
-    fun print() {
-        println("Hello, World!")
+    fun result(foo: String, bar: String, Function:(String, String) -> String): String{
+        val result = Function(foo, bar)
+        return """<h1>$foo</h1><br>h1>$bar</h1><br>$result"""
     }
+
+    fun marinafunc(foo: String, bar: String): String="Σ="+(Integer.valueOf(foo)+Integer.valueOf(bar)).toString()
+    fun guest(a: String, b: String): String=""
+
+    fun callmarina(){
+        val foo = "3"
+        val bar = "7"
+        println(result(foo,bar, ::marinafunc))
+        println(result(foo,bar, ::guest))
+    }
+
 }
